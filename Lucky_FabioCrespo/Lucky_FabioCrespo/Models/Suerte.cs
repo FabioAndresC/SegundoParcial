ee2007a498880f38df6d3f7efe8dbe22aa3cb6c9
Fabio <a2020115158@estudiantes.upsa.edu.bo>
2021-10-13 19:35:46 -0400

    Se agrego la carpeta Models y la clase Suerte

---
+++ Lucky_FabioCrespo/Lucky_FabioCrespo/Models/Suerte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_FabioCrespo.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public int SuerteId { get; set; }
+        [Required]
+        [StringLength(1000, MinimumLength = 5, ErrorMessage = "La suerte debe contener entre 5 a 100 caracteres")]
+        public string Detalle { get; set; }
+        [Url]
+        [Required]
+        public string Imagen { get; set; }
+
+    }
+}
