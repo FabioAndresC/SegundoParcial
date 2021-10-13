5b82d9253344c58b8929ffb48bbe97b25046c7f1
Fabio <a2020115158@estudiantes.upsa.edu.bo>
2021-10-13 19:59:40 -0400

    Se agregó LuckyController

---
+++ Lucky_FabioCrespo/Lucky_FabioCrespo/Controllers/LuckyController.cs
@@ -0,0 +1,39 @@
+﻿using Lucky_FabioCrespo.Data;
+using Lucky_FabioCrespo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_FabioCrespo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuckyController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public LuckyController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Suerte>> GetSuerte()
+        {
+            var list = await _context.Suerte.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var suerte = list[index];
+
+            if(suerte == null)
+            {
+                return NoContent();
+            }
+            return suerte;
+        }
+    }
+}
