aaebc86daa9afcd636f9d684a965d158f326fd74
Fabio <a2020115158@estudiantes.upsa.edu.bo>
2021-10-13 19:44:29 -0400

    Se creo el directorio Data y la clase AppDbContext, para luego agregarla a Startup.cs

---
+++ Lucky_FabioCrespo/Lucky_FabioCrespo/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Lucky_FabioCrespo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_FabioCrespo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar Model -> Cancion
+        public DbSet<Suerte> Suerte { get; set; }
+    }
+}
